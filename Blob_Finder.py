7c77c867814238f49e344a72e4031ff8677c4870
HSMSC <144365476+HSMSC@users.noreply.github.com>
2023-09-09 02:24:53 -0700

    Update Blob_Finder.py

--- Blob_Finder.py
+++ Blob_Finder.py
@@ -24,43 +24,43 @@
 ...         params.maxThreshold = 200
 
 
-        # Filter by Area.
-        params.filterByArea = True
-        params.minArea = 50
+            # Filter by Area.
+            params.filterByArea = True
+            params.minArea = 50
 
-        # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = 0.1
+            # Filter by Circularity
+            params.filterByCircularity = True
+            params.minCircularity = 0.1
 
-        # Filter by Convexity
-        params.filterByConvexity = True
-        params.minConvexity = 0.87
+            # Filter by Convexity
+            params.filterByConvexity = True
+            params.minConvexity = 0.87
             
-        # Filter by Inertia
-        params.filterByInertia = True
-        params.minInertiaRatio = 0.01
+            # Filter by Inertia
+            params.filterByInertia = True
+            params.minInertiaRatio = 0.01
 
-        # Create a detector with the parameters
-        ver = (cv2.__version__).split('.')
-        if int(ver[0]) < 3 :
+            # Create a detector with the parameters
+            ver = (cv2.__version__).split('.')
+            if int(ver[0]) < 3 :
                 detector = cv2.SimpleBlobDetector(params)
-        else : 
+            else : 
                 detector = cv2.SimpleBlobDetector_create(params)
 
 
-        # Detect blobs.
-        keypoints = detector.detect(im)
+            # Detect blobs.
+            keypoints = detector.detect(im)
 
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-        # the size of the circle corresponds to the size of blob
+            # Draw detected blobs as red circles.
+            # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+            # the size of the circle corresponds to the size of blob
 
-        im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # Compute average size and number of blobs detected
-        avg_size = sum([x.size for x in keypoints]) / len(keypoints)
-        num_points = len(keypoints)
+            im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            # Compute average size and number of blobs detected
+            avg_size = sum([x.size for x in keypoints]) / len(keypoints)
+            num_points = len(keypoints)
 
-        return (avg_size, num_points)
+            return (avg_size, num_points)
 
 #Note: all above are indented (inside the function, after defining process_image). If you get an indentation error, type ipython and try again.
 
